ba824e972a04a34003b829af82c439911e17a804
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-16 08:35:34 -0500

    Woohoo! Rake task successfully creates a colormap with the 774 colors we have in the DB.

--- lib/generate_colormap.rb
+++ lib/generate_colormap.rb
@@ -2,9 +2,15 @@ require 'json'
 
 module GenerateColormap
   def start(colors)
-    # Let's start by grabbing all the colors we'll need.
+    date_string = Date.today.strftime("%Y-%m-%d")
+    filepath    = "lib/assets/images/colormap_#{date_string}.png"
+    # alright, this is going to be a fucking long terminal command. Hopefully there aren't limits.
+    query_string = "convert -size #{colors.length}x1 xc:white "
+    colors.each_with_index do |c, i|
+      query_string += "-fill '#{c}' -draw 'point #{i},0' "
+    end
+    query_string += filepath
 
-
-    puts colors
+    system(query_string)
   end 
 end
