8e87f556a5f1621a6d44934264f987918179d8a9
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-17 09:23:22 -0500

    Added conversion from RGB to XYZ, and spec.

--- app/services/colour/convert.rb
+++ app/services/colour/convert.rb
@@ -7,6 +7,7 @@ class Colour::Convert
     if from_type == :rgb
       new_color = rgb_to_hsl(colour) if to_type == :hsl
       new_color = rgb_to_lab(colour) if to_type == :lab
+      new_color = rgb_to_xyz(colour) if to_type == :xyz 
     elsif from_type == :hsl
       # do me
     elsif from_type == :lab
@@ -27,6 +28,8 @@ class Colour::Convert
       type = :hsl
     elsif colour.key(:l) && colour.key(:a) && colour.key?(:b)
       type = :lab
+    elsif colour.key(:x) && colour.key(:y) && colour.key?(:z)
+      type = :xyz
     else
       raise "Invalid color type (colour needs to have keys for either :r, :g, :b OR :h, :s, :l OR :l, :a, :b)"
     end
@@ -74,4 +77,92 @@ class Colour::Convert
       l: l
     }
   end
+# function RGBtoXYZ(R, G, B)
+# {
+#     var_R = parseFloat( R / 255 )        //R from 0 to 255
+#     var_G = parseFloat( G / 255 )        //G from 0 to 255
+#     var_B = parseFloat( B / 255 )        //B from 0 to 255
+
+#     if ( var_R > 0.04045 ) var_R = ( ( var_R + 0.055 ) / 1.055 ) ^ 2.4
+#     else                   var_R = var_R / 12.92
+#     if ( var_G > 0.04045 ) var_G = ( ( var_G + 0.055 ) / 1.055 ) ^ 2.4
+#     else                   var_G = var_G / 12.92
+#     if ( var_B > 0.04045 ) var_B = ( ( var_B + 0.055 ) / 1.055 ) ^ 2.4
+#     else                   var_B = var_B / 12.92
+
+#     var_R = var_R * 100
+#     var_G = var_G * 100
+#     var_B = var_B * 100
+
+#     //Observer. = 2°, Illuminant = D65
+#     X = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805
+#     Y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722
+#     Z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505
+#     return [X, Y, Z]
+# }
+
+  # this is typically the middle step between RGB and LAB.
+  def self.rgb_to_xyz(colour)
+    r_prime = colour[:r] / 255.0
+    g_prime = colour[:g] / 255.0
+    b_prime = colour[:b] / 255.0
+
+    new_r = r_prime > 0.04045 ? ( ( r_prime + 0.055 ) / 1.055 ) ** 2.4 : r_prime / 12.92
+    new_g = g_prime > 0.04045 ? ( ( g_prime + 0.055 ) / 1.055 ) ** 2.4 : g_prime / 12.92
+    new_b = b_prime > 0.04045 ? ( ( b_prime + 0.055 ) / 1.055 ) ** 2.4 : b_prime / 12.92
+
+    {
+      x: (new_r * 0.4124 + new_g * 0.3576 + new_b * 0.1805) * 100,
+      y: (new_r * 0.2126 + new_g * 0.7152 + new_b * 0.0722) * 100,
+      z: (new_r * 0.0193 + new_g * 0.1192 + new_b * 0.9505) * 100
+    }
+  end
+
+  def self.rgb_to_lab(colour)
+
+
+  end
 end
+
+# // user colour
+# var Red   = 56;
+# var Green = 79;
+# var Blue  = 132;
+
+# // user colour converted to XYZ space
+# XYZ = RGBtoXYZ(Red,Green,Blue)
+# var colX = XYZ[0];
+# var colY = XYZ[1];
+# var colZ = XYZ[2];
+
+# // alert(XYZ)
+
+# LAB = XYZtoLAB(colX, colY, colZ)
+
+# alert(LAB)
+
+
+
+# function XYZtoLAB(x, y, z)
+# {
+#     var ref_X =  95.047;
+#     var ref_Y = 100.000;
+#     var ref_Z = 108.883;
+
+#     var_X = x / ref_X          //ref_X =  95.047   Observer= 2°, Illuminant= D65
+#     var_Y = y / ref_Y          //ref_Y = 100.000
+#     var_Z = z / ref_Z          //ref_Z = 108.883
+
+#     if ( var_X > 0.008856 ) var_X = var_X ^ ( 1/3 )
+#     else                    var_X = ( 7.787 * var_X ) + ( 16 / 116 )
+#     if ( var_Y > 0.008856 ) var_Y = var_Y ^ ( 1/3 )
+#     else                    var_Y = ( 7.787 * var_Y ) + ( 16 / 116 )
+#     if ( var_Z > 0.008856 ) var_Z = var_Z ^ ( 1/3 )
+#     else                    var_Z = ( 7.787 * var_Z ) + ( 16 / 116 )
+
+#     CIE_L = ( 116 * var_Y ) - 16
+#     CIE_a = 500 * ( var_X - var_Y )
+#     CIE_b = 200 * ( var_Y - var_Z )
+
+# return [CIE_L, CIE_a, CIE_b]
+# }
--- spec/services/colour/convert_spec.rb
+++ spec/services/colour/convert_spec.rb
@@ -4,25 +4,48 @@ RSpec.describe Colour::Convert do
   describe "converts from one color format to another" do
     describe "Converts RGB to HSL" do
       it "converts when red is max, and green is more than blue" do
-        expect(Colour::Convert.call({r: 211, g: 200, b: 50 }, :hsl)).to eq({h: 56,  s: 65,  l: 51}) # Red max, green > blue
+        expect(Colour::Convert.call({r: 211, g: 200, b: 50 }, :hsl)).to eq({h: 56,  s: 65,  l: 51}) 
       end
 
       it "converts when red is max, and green is less than blue" do
-        expect(Colour::Convert.call({r: 211, g: 36,  b: 125}, :hsl)).to eq({h: 329, s: 71,  l: 48}) # Red max, green < blue
+        expect(Colour::Convert.call({r: 211, g: 36,  b: 125}, :hsl)).to eq({h: 329, s: 71,  l: 48}) 
       end
 
       it "converts when green is max" do
-        expect(Colour::Convert.call({r: 60,  g: 178, b: 125}, :hsl)).to eq({h: 153, s: 50,  l: 47}) # Green max
+        expect(Colour::Convert.call({r: 60,  g: 178, b: 125}, :hsl)).to eq({h: 153, s: 50,  l: 47}) 
       end
 
       it "converts when blue is max" do
-        expect(Colour::Convert.call({r: 11,  g: 0,   b: 157}, :hsl)).to eq({h: 244, s: 100, l: 31}) # Blue max
+        expect(Colour::Convert.call({r: 11,  g: 0,   b: 157}, :hsl)).to eq({h: 244, s: 100, l: 31}) 
       end
 
       it "converts when greyscale" do
-        expect(Colour::Convert.call({r: 60,  g: 60,  b: 60},  :hsl)).to eq({h: 0,   s: 0,   l: 24}) # Greyscale
+        expect(Colour::Convert.call({r: 60,  g: 60,  b: 60},  :hsl)).to eq({h: 0,   s: 0,   l: 24}) 
       end
       
     end
+    describe "Converts RGB to XYZ" do
+      it "converts when RGB are all over 11" do
+        expect(Colour::Convert.call({r: 211, g: 200, b: 50 }, :xyz)).to eq({x: 48.09396843392571, y: 55.38772630986121, z: 11.173689673731442})
+      end
+
+      it "converts when red is less than 11" do
+        expect(Colour::Convert.call({r: 5, g: 36, b: 125}, :xyz)).to eq({x: 4.395134748357606, y: 2.774685980098559, z: 19.70595502679241})
+      end
+
+      it "converts when green is less than 11" do
+        expect(Colour::Convert.call({r: 60, g: 0, b: 125}, :xyz)).to eq({x: 5.56515026071048, y: 2.44132718197755, z: 19.579943268363976})
+      end
+
+      it "converts when blue is less than 11" do
+        expect(Colour::Convert.call({r: 245, g: 122, b: 2}, :xyz)).to eq({x: 44.626679340657475, y: 33.335927499952035, z: 4.1398254163975166})
+      end
+
+      it "converts when RGB are all under 11" do
+        expect(Colour::Convert.call({r: 2, g: 4, b: 6},  :xyz)).to eq({x: 0.10132337764827293, y: 0.11288775572148363, z: 0.1887452194500091})
+      end
+      
+    end
+
   end
 end
