decd25816ca6b56958592c77de87b89c2bd3ae7c
Josh Comeau <joshwcomeau@gmail.com>
2014-12-18 14:13:34 -0500

    Refactoring the photoscontroller. Got it a little more organized. Need to use HSV instead of HSL, though.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,23 +5,21 @@ class PhotosController < ApplicationController
     
     return render json: {status: 'error', message: "Couldn't save photo to disk."} unless path = save_photo_to_disk(params[:photo])
 
+    filename = params[:photo].original_filename
+
     # Let's make a histogram at 64 colours.
-    histogram_string = `
-      convert #{path}   \
-      -format %c        \
-      -colors 64        \
-      histogram:info:- | sort -n -r`
+    histogram = make_histogram(path, 64)
 
+    colour_list = get_colour_list(histogram)
 
-    colour_list = get_colour_list(histogram_string)
     # Returns an array of arrays, with occurance and RGB:
     # [  [20000, 255,255,255], [19000, 128,52,66], [18000, 0,0,0]  ]
 
     hsl_colour_list = get_hsl_colours(colour_list)
 
-    hues        = get_channel_stats(hsl_colour_list, :h)
-    saturations = get_channel_stats(hsl_colour_list, :s)
-    lightnesses = get_channel_stats(hsl_colour_list, :l)
+    hues            = get_channel_stats(hsl_colour_list, :h)
+    saturations     = get_channel_stats(hsl_colour_list, :s)
+    lightnesses     = get_channel_stats(hsl_colour_list, :l)
 
 
     
@@ -34,41 +32,46 @@ class PhotosController < ApplicationController
     puts "LIGHTNESS OUTLIERS:"
     puts lightnesses[:outliers]
 
+    eight_colour_histogram = make_histogram(path, 8)
+    eight_colour_list      = get_colour_list(eight_colour_histogram)
+
+    # For now: Make an image with the top 4 colors + outliers
+    final_colours = eight_colour_list
     
+    # For saturations, we only want outliers ABOVE the mean.
+    saturations[:outliers].each do |s|
+      final_colours.push(s) if s[:hsl][:s] > saturations[:mean]
+    end
+
+    # Same for lightness
+    lightnesses[:outliers].each do |l|
+      final_colours.push(l) if l[:hsl][:l] > lightnesses[:mean]
+    end
+
+    # For hue, we'll just take them all
+    hues[:outliers].each do |h|
+      final_colours.push(h)
+    end
 
 
-    render json: {}
+    render json: final_colours
   end
 
   private
 
+  def make_histogram(path, colours)
+    `convert #{path}   \
+    -format %c         \
+    -colors #{colours} \
+    histogram:info:- | sort -n -r`
+  end
+
   def get_channel_stats(all_colours, channel)
-    ### DESIRED FORMAT - 
-    # {
-    #   deviation: 25,
-    #   mean:      49,
-    #   colours: [
-    #     {
-    #       value:   75,
-    #       z_score: 3.45
-    #     },
-    #     {
-    #       value:   34,
-    #       z_score: 0.25
-    #     }
-    #   ]
-    # }
-    
-    colours   = all_colours.map { |c| c[channel]}
+    colours   = all_colours.map { |c| c[:hsl][channel]}
     mean      = Maths.mean(colours)
     deviation = Maths.standard_deviation(colours)
-    outliers  = get_outliers(colours, mean, deviation)
+    outliers  = get_outliers(all_colours, colours, channel, mean, deviation)
 
-    # Let's find the color(s) associated with our outliers and turn 'em back to RGB.
-    outliers.map! do |o|
-      matching_colours = all_colours.select { |c| c[channel] == o }
-      matching_colours.map { |c| Colour::Convert.call(c, :rgb)}
-    end
 
     return {
       colours:   colours,
@@ -79,8 +82,18 @@ class PhotosController < ApplicationController
 
   end
 
-  def get_outliers(colours, mean, deviation, threshold=2)
-    outliers = colours.select { |c| Maths.z_score(c, colours, mean, deviation).abs > threshold}
+  def get_outliers(all_colours, measuring_colours, channel, mean, deviation, threshold=2)
+    outliers = []
+    all_colours.each do |c| 
+      z_score = Maths.z_score(c[:hsl][channel], measuring_colours, mean, deviation)
+      if z_score > threshold
+        outliers.push({
+          color: c,
+          z_score: z_score
+        })
+      end
+    end
+    outliers
   end
 
 
@@ -95,14 +108,20 @@ class PhotosController < ApplicationController
       # First, we need to get this in the right format.
       # From  ['255','255','255'] 
       # to    { r: 255, g: 255, b: 255 }
-      formatted_rgb = {
+      rgb = {
         r: color_array[1].to_i,
         g: color_array[2].to_i,
         b: color_array[3].to_i
       }
 
       # Next, we do the conversion
-      Colour::Convert.call(formatted_rgb, :hsl)
+      hsl = Colour::Convert.call(rgb, :hsl)
+
+      {
+        occurances: color_array[0].to_i,
+        rgb:        rgb,
+        hsl:        hsl
+      }
     end
   end
 
