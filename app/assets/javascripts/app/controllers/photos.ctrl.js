e85f6a383a3656352fbbd3d5b4115f5b1175b012
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-19 17:25:33 -0500

    Managed to adapt the file upload to use controllerAs, and extracted the logic into a service.

--- app/assets/javascripts/app/controllers/photos.ctrl.js
+++ app/assets/javascripts/app/controllers/photos.ctrl.js
@@ -1,32 +1,24 @@
-function PhotosController($scope, $upload) {
-  $scope.$watch('photo', function() {
-    var file = $scope.photo;
-    $scope.upload = $upload.upload({
-      url: '/photos/', // upload.php script, node.js route, or servlet url
-      method: 'POST',
+function PhotosController($scope, $attrs, Photos) {
+  this.photo = null;
+  this.auth  = $attrs.authToken;
 
-      data: {photo: $scope.photo},
-      file: file, // single file or a list of files. list is only for html5
-      //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-      fileFormDataName: "photo", 
-      //formDataAppender: function(formData, key, val){}  // customize how data is added to the formData. 
-                                                          // See #40#issuecomment-28612000 for sample code
+  var ctrl   = this;
 
-    }).progress(function(evt) {
-      console.log(evt);
-    }).success(function(data, status, headers, config) {
-      // file is uploaded successfully
-      console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-    });
-    //.error(...)
-    //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
-    //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to the underlying XMLHttpRequest
+  $scope.$watch(angular.bind(this, function() {
+    return this.photo;
+  }), function(newVal, oldVal) {
+    console.log("Watch triggered with ", newVal);
+    if (newVal) {
+      $scope.upload = Photos.upload(newVal, ctrl.auth);
+    }
   });
+
+
 }
 
 
 
 
 
-PhotosController.$inject = ['$scope', '$upload'];
-angular.module('colourMatch').controller('PhotosController', ['$scope', '$upload', PhotosController]);
+PhotosController.$inject = ['$scope', '$attrs', 'Photos'];
+angular.module('colourMatch').controller('PhotosController', ['$scope', '$attrs', 'Photos', PhotosController]);
--- app/assets/javascripts/app/services/photos.service.js
+++ app/assets/javascripts/app/services/photos.service.js
@@ -1,5 +1,31 @@
-function Photos($resource) {
-  return $resource('/photos/');
+function Photos($upload) {
+  var serv = this;
+
+  this.upload = function(photo, token) {
+    return $upload.upload({
+      url: '/photos/', // upload.php script, node.js route, or servlet url
+      method: 'POST',
+      data: {photo: photo, authenticity_token: token},
+      file: photo, 
+      fileFormDataName: "photo", 
+      formDataAppender: function(formData) {
+        formData.append("authenticity_token", token);
+        formData.append("photo", photo);
+      } 
+
+    }).progress(function(evt) {
+      console.log(evt);
+    }).success(function(data, status, headers, config) {
+      // file is uploaded successfully
+      console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
+    });
+    //.error(...)
+    //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
+    //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to the underlying XMLHttpRequest
+
+  };
+
+
 }
 
-angular.module('colourMatch').factory("Photos", ["$resource", Photos]);
+angular.module('colourMatch').service("Photos", ["$upload", Photos]);
