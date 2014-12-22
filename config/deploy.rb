90a8cf1f543cd9edd99fbdff90a533aef3a671c7
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-22 17:47:11 -0500

    Fixed path to puma.sock

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :rbenv_roles, :all # default value
 
 
 
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
