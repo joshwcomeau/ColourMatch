1305d931576fd8b73d4f66979fce735ab9b4cb61
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-23 10:46:41 -0500

    YES. Got it working.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,14 +7,45 @@ class PhotosController < ApplicationController
   # Param: colour  -> A single hex colour code
   #        colours -> A comma-separated list of 6 hex colour codes
   def index
-
-
+    col = Colour.all.sample(30)
     response.headers['Content-Type']  = 'text/event-stream'
-    response.stream.write("data: 333333\n\n")
-    response.stream.write("data: 999999\n\n")
-    response.stream.write("data: DDDDDD\n\n")
-    response.stream.write("data: OVER\n\n")
+
+    print "After getting cols, we have #{col.length}\n\n"
+    col.each do |c|
+      response.stream.write("data: #{c.hex}\n\n")  
+      1_000_000.times do |n|
+        n * 1000
+      end
+    end
+    print "After writing 3 colors to stream, we have #{col.length}\n\n"
+    
+    response.stream.write("data: OVER\n\n")      
+    print "After sleeping 1 and writing close to stream, we have #{col.length}\n\n"
     response.stream.close
+    print "After closing to stream, we have #{col.length}\n\n"
+
+
+
+    # begin
+
+      
+
+    #   puts "response: #{response}"
+    #   puts "response headers: #{response.headers}"
+
+    #   # Colour.first(10).each do |c|
+    #   #   puts "Now returning #{c.hex}"
+    #   #   sse.write(c.hex)
+    #   #   sleep 1
+    #   # end
+    # rescue Exception => e
+    #   puts "Rescuing! #{e}"
+    #   IOError
+    # ensure
+    #   puts "Connection terminating."
+    #   response.stream.write("data: OVER\n\n")      
+    #   response.stream.close
+    # end
 
 
   end
