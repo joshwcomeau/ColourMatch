f7550c6f2d51a046005d8232ccd439b286349251
Josh Comeau <joshwcomeau@gmail.com>
2014-12-26 07:17:21 -0500

    Improved Colour::FindClosest performance; reduced benchmark time from 3.5s to 1s, using bins. Woohoo!

--- app/services/colour/find_closest.rb
+++ app/services/colour/find_closest.rb
@@ -1,17 +1,37 @@
 class Colour::FindClosest
-  def self.call(colour, subset=nil)
+  def self.call(colour, subset=nil, use_bins=true)
     # We want to find which of the colors in our DB is closest to the provided color.
     # So, we need start by converting to LAB if it isn't already, for accuracy.
     # Then, do some pythagorean math.
     colour = Colour::GetLabColour.call(colour)
 
-    subset ||= Colour.all
-
-    get_nearest_colour(colour, subset)
-
+    if use_bins
+      bin = get_nearest_bin(colour)
+      get_nearest_colour(colour, bin.colours)
+    else
+      subset ||= Colour.all
+      get_nearest_colour(colour, subset)
+    end
   end
 
+
+
   private
+
+  def self.get_nearest_bin(colour)
+    closest_bin = nil
+    closest_distance = 1_000_000
+
+    Bin.includes(:exemplar).each do |b|
+      distance = Colour::CalculateDistance.call(colour, b.exemplar[:lab])
+      if distance < closest_distance
+        closest_distance = distance
+        closest_bin      = b 
+      end      
+    end
+
+    closest_bin
+  end
 
   def self.get_nearest_colour(c1, subset)
     closest_color = nil
