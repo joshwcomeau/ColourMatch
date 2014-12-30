f20583f9e54b99d8e980a68d6675aa095cb183da
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-30 10:16:53 -0500

    Updating the variable name to reflect 3-bit instead of 4-bit.

--- app/services/photo/create_palette_from_photo.rb
+++ app/services/photo/create_palette_from_photo.rb
@@ -4,15 +4,15 @@ class Photo::CreatePaletteFromPhoto
     colour_data_6_bit      = Photo::GetHistogramData.call(path, colours: 64, resize: resize)
     hsb_channel_data_6_bit = Photo::GetHSBChannelStats.call(colour_data_6_bit)
     
-    # Let's get 4-bit (16-colour) data
-    colour_data_4_bit      = Photo::GetHistogramData.call(path, colours: 8, resize: resize)
-    # hsb_channel_data_4_bit = Photo::GetHSBChannelStats.call(colour_data_4_bit)
+    # Let's get 3-bit (16-colour) data
+    colour_data_3_bit      = Photo::GetHistogramData.call(path, colours: 8, resize: resize)
+    # hsb_channel_data_4_bit = Photo::GetHSBChannelStats.call(colour_data_3_bit)
 
 
     ####### Current strategy:  #########################################################
     ####### Combine the 4-6 most popular 16-bit colors, as well as 0-2 outliers ########
 
-    commons  = Photo::ExtractMostCommonColours.call(colour_data_4_bit)
+    commons  = Photo::ExtractMostCommonColours.call(colour_data_3_bit)
     outliers = Photo::ExtractOutliers.call(hsb_channel_data_6_bit)
 
     results  = Photo::CompileToDominant.call(commons, outliers)
