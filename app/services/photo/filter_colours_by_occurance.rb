91d17bfbc44f2056bf86cba77df66785ea15d828
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-01 19:21:51 -0500

    NEW YEARS REFACTORING: Fixed a couple bugs with ExtractOutliers service. All specs passing now.

--- app/services/photo/filter_colours_by_occurance.rb
+++ app/services/photo/filter_colours_by_occurance.rb
@@ -1,6 +1,7 @@
 class Photo::FilterColoursByOccurance
   def self.call(colour_data, minimum: 0.001)
     min_occurance = colour_data[:pixels] * minimum
+
     colour_data[:colours].select { |c| c[:occurances] >= min_occurance }
   end
 end
--- app/services/photo/get_hsb_channel_stats.rb
+++ app/services/photo/get_hsb_channel_stats.rb
@@ -3,22 +3,20 @@
 
 class Photo::GetHSBChannelStats
   
-  def self.call(colours)
+  def self.call(colour_data)
     [
-      get_channel_stats(colours, :h),
-      get_channel_stats(colours, :s),
-      get_channel_stats(colours, :b)
+      get_channel_stats(colour_data, :h),
+      get_channel_stats(colour_data, :s),
+      get_channel_stats(colour_data, :b)
     ]
   end
 
   private
 
-  def self.get_channel_stats(all_colours, channel)
-
-    colours   = build_representative_array(all_colours, channel)
+  def self.get_channel_stats(colour_data, channel)
+    colours   = build_representative_array(colour_data, channel)
     mean      = Maths.mean(colours)
     deviation = Maths.standard_deviation(colours)
-    # outliers  = get_outliers(all_colours, colours, channel, mean, deviation)
 
     return {
       channel:   channel,
@@ -31,26 +29,8 @@ class Photo::GetHSBChannelStats
   # I want to take into account how many times a colour is repeated, when getting deviation.
   # However, it adds too much to the processing time to use every pixel, so I'm dividing 
   # occurances by 500 so get a general representation of the occurances, without killing CPU.
-  def self.build_representative_array(all_colours, channel)
-    all_colours.map { |c| [c[:hsb][channel]] * (c[:occurances] / 500.0).ceil}.flatten
-  end
-
-  # No longer used. Moved to Photo::ExtractOutliers
-  def self.get_outliers(all_colours, measuring_colours, channel, mean, deviation, threshold=1)
-    outliers = []
-    all_colours.each do |c| 
-
-      # z_score = Maths.z_score(c[:hsb][channel], measuring_colours, mean, deviation).abs
-      if z_score > threshold
-
-        # For saturation and brightness, we only want colors ABOVE the mean (positive z-score)
-        if channel == :h || c[:hsb][channel] > mean
-          c[:z_score] = z_score
-          outliers.push(c)
-        end
-      end
-    end
-    outliers
+  def self.build_representative_array(colours, channel)
+    colours.map { |c| [c[:hsb][channel]] * (c[:occurances] / 500.0).ceil}.flatten
   end
 
 end
