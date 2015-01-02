f6df35dc35d3b3dd353b841780c43d952941eed7
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-01 20:12:18 -0500

    NEW YEARS REFACTORING: Slight tweak to euclidean distance formula in Colour::CalculateDistance. Reduced lightness' relative importance, since hue/saturation is more important.

--- app/services/colour/calculate_distance.rb
+++ app/services/colour/calculate_distance.rb
@@ -9,6 +9,6 @@ class Colour::CalculateDistance
   private
 
   def self.calculate_distance(c1, c2)
-    ( (c1[:l] - c2[:l])**2 + (c1[:a] - c2[:a])**2 + (c1[:b] - c2[:b])**2 ) ** 0.5
+    ( (c1[:l] - c2[:l])**2*0.7 + (c1[:a] - c2[:a])**2 + (c1[:b] - c2[:b])**2 ) ** 0.5
   end
 end
