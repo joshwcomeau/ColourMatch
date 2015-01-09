1c18e25bba68dcb5b955c18b91bae6db7791eaad
Josh Comeau <joshwcomeau@gmail.com>
2015-01-09 11:21:58 -0500

    Added the structure for the suggested photos on the dashboard. Still needs to be styled and have its functionality implemented.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
-
+    # On page load we want to throw a handful of suggestions
+    @suggestions  = Photo.where(from_500px: false).order("created_at DESC").first(4)
+    @column_width = 12 / @suggestions.count
   end
 end
