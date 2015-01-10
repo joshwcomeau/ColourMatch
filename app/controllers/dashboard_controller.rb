c317ccb8fe4b21c3bb84cd950aace60df2e2859e
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-10 12:07:24 -0500

    BUGFIX: Fixed the homepage in the event that there are no user-uploaded photos to draw on for suggestions.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def index
     # On page load we want to throw a handful of suggestions
     @suggestions  = Photo.where(from_500px: false).order("created_at DESC").first(4)
-    @column_width = 12 / @suggestions.count
+    
+    @column_width = @suggestions.any? ? (12 / @suggestions.count) : 12
   end
 end
