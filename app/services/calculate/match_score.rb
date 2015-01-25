8135e91417fa8ef2b2a845f26834cc40806f64bf
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-25 12:07:27 -0500

    Refactored Calculate::MatchScore, added a couple more photo options (although it turns out the current option is best).

--- app/services/calculate/match_score.rb
+++ app/services/calculate/match_score.rb
@@ -14,39 +14,18 @@ class Calculate::MatchScore
         score = 0
 
 
-        # What if we just grab photos where the most saturated colours line up, and the mean brightness is about the same?
-        common_data   = get_most_common(data.photo_colours)
-        common_target = get_most_common(p2.photo_colours)
-        score_part_1 = normalized_dist(common_data, common_target)
+        # We take the most common colour, and the most saturated/bright colour, and just compare those for both p1 and p2
+        score = by_saturation_and_common(data, p2)
 
+        # We consider any non-grey colour that has a match score over 88% a 'match'.
+        # We need at least half the colours to be matches for it to be returned, and we return the average match.
+        # score = by_threshold_num_of_matches(data, p2)
+
+
+        # We find the highest p2 match for each of our p1 colours regardless of anything, and return the average match score.
+        # score = by_average_match(data, p2)
         
-        saturated_data   = get_most_saturated(data.photo_colours)
-        saturated_target = get_most_saturated(p2.photo_colours)
-        score_part_2 = normalized_dist(saturated_data, saturated_target)
 
-
-        score = ((score_part_1 + score_part_2) / 2).round(2)
-        
-        # We only want to compare colours that aren't greyscale, or near it.
-        # good_data_colours   = data.photo_colours.select { |c| c.hsb["s"] > 18 }
-        # good_target_colours = p2.photo_colours.select   { |c| c.hsb["s"] > 18 }
-
-
-        # if good_target_colours.any?
-        #   good_data_colours.each do |c|
-        #     closest = get_nearest_colour(c, good_target_colours)
-        #     dist = normalized_dist(c, closest)
-        #     if dist >= 88
-        #       matching_colours << dist
-        #     end
-        #   end
-        # end
-
-        # # We consider it a match if at least half of the colours match
-        # if matching_colours.count >= (good_data_colours.count * 0.5).ceil
-
-        #   score = Maths.mean(matching_colours).round(2)
-        # end
 
 
       end
@@ -61,6 +40,56 @@ class Calculate::MatchScore
   end
 
   private
+
+  def self.by_average_match(p1, p2)
+    scores = []
+    p1.photo_colours.each do |c1|
+      best_match = 0
+      p2.photo_colours.each do |c2|
+        current_match = normalized_dist(c1, c2)
+
+        best_match = current_match if current_match > best_match
+      end
+
+      scores << best_match
+    end
+
+    Maths.mean(scores).round(2)
+  end
+
+  def self.by_threshold_num_of_matches(p1, p2)
+    # We only want to compare colours that aren't greyscale, or near it.
+    good_p1_colours = p1.photo_colours.select { |c| c.hsb["s"] > 18 }
+    good_p2_colours = p2.photo_colours.select { |c| c.hsb["s"] > 18 }
+
+    if good_p2_colours.any?
+      good_p1_colours.each do |c|
+        closest = get_nearest_colour(c, good_p2_colours)
+        dist    = normalized_dist(c, closest)
+
+        if dist >= 88
+          matching_colours << dist
+        end
+      end
+    end
+
+    # We consider it a match if at least half of the colours match
+    if matching_colours.count >= (good_p1_colours.count * 0.5).ceil
+      score = Maths.mean(matching_colours).round(2)
+    end  
+  end
+
+  def self.by_saturation_and_common(p1, p2)
+    common_p1     = get_most_common(p1.photo_colours)
+    common_p2     = get_most_common(p2.photo_colours)
+    score_part_1  = normalized_dist(common_p1, common_p2)
+
+    saturated_p1  = get_most_saturated(p1.photo_colours)
+    saturated_p2  = get_most_saturated(p2.photo_colours)
+    score_part_2  = normalized_dist(saturated_p1, saturated_p2)
+
+    ((score_part_1 + score_part_2) / 2).round(2)
+  end
 
   def self.get_most_common(colours)
     colours.sort { |a, b|  b.coverage <=> a.coverage }.first
