4ebda43da9bf7d51c31e90df05c53e20db62dc8a
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-30 10:37:38 -0500

    Finished tweaking Maths::circular_mean

--- lib/maths.rb
+++ lib/maths.rb
@@ -41,24 +41,25 @@ module Maths
     average_y = mean( cartesian_array.map { |arr| arr[1] } )
 
     # let's convert this x,y pair back to polar coordinates, for our solution.
-    convert_to_polar(average_x, average_y)
+    polar = convert_to_polar(average_x, average_y)
+    polar[:angle]
   end
 
   def self.convert_to_cartesian(angle, radius: 1, type: :radians)
     # If supplied with the angle in degrees, we need to convert to radians.
     angle *= DEG_MULTIPLIER
-    
+
     # We're using the unit circle by default for all calculations; radius = 1
     [ radius * Math::cos(angle) , radius * Math::sin(angle) ]
   end 
 
 
   def self.convert_to_polar(x, y)
-    # pretty sure r will be equal to 1, since that's what all the values are, but just in case let's do the math
+    # we don't need the radius for calculating circular_mean, but what the hell.
     r = (x**2 + y**2)**0.5
 
     radians = Math::atan(y/x)
-    angle = radians * RAD_MULTIPLIER
+    angle   = radians * RAD_MULTIPLIER
 
     case find_quadrant(x, y)
     when 2 || 3
@@ -67,7 +68,10 @@ module Maths
       angle += 360
     end
 
-    [r, angle]
+    {
+      r:     r,
+      angle: angle
+    }
   end 
 
 
